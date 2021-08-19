d04828da98d2eb938cf83f7ca959ba378d3f2625
mashrurbd <mohsinmashrur@gmail.com>
2021-08-20 01:15:49 +0600

    Added test py

--- script.py
+++ script.py
@@ -13,18 +13,20 @@ headers = {'Authorization' : '', 'Accept' : 'application/json', 'Content-Type' :
 def post_request():
     while True:
         rdata = requests.post(url, data=open('payload.json', 'rb'), headers=headers)
-        print(rdata.text)
+        # print(rdata.text)
+        j = json.loads(rdata.text)
+        print(j['data']['updatePoll']['id'] : '')
 
 threads = [] 
-for i in range(50): 
+for i in range(500): 
     t = threading.Thread(target=post_request) 
     t.daemon = True 
     threads.append(t) 
 
-for i in range(50): 
+for i in range(500): 
     threads[i].start() 
 
-for i in range(50):
+for i in range(500):
     threads[i].join() 
 
 
---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import json
+import requests
+import threading
+
+url = 'https://radar.somoynews.tv/graphql'
+
+headers = {'Authorization' : '', 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+
+
+rdata = requests.post(url, data=open('payload.json', 'rb'), headers=headers)
+j = json.loads(rdata.text)
+print(j)
+print(j['data']['updatePoll']['id'] + " : " + j['data']['updatePoll']['op'] + " votes.")
+
+
+
